851ea25a124917b72e19703c6965941a054f9499
Kabryn Hodgson <kjhodgson@tlu.edu>
2017-10-17 13:51:03 -0500

    Added main file names for project

---
+++ MatrixServer/MatrixServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
