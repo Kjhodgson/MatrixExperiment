479f31c701cd27b71e32a98af38f300bc867078c
Kabryn Hodgson <kjhodgson@tlu.edu>
2017-10-17 13:57:41 -0500

    Using a free to use program to test server client activities.

--- MatrixClient/MatrixClient/Program.cs
+++ MatrixClient/MatrixClient/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
-
-namespace MatrixClient
+﻿namespace MatrixClient
 {
-    class Program
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+    using System.Net.Sockets;
+
+
+    public class client
     {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("10.20.133.186", 8001);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
         }
+
     }
 }
