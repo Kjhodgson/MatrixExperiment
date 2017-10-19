14b81d030e616b2ee22b2f25064f5b02bebe2bbb
Kabryn Hodgson <kjhodgson@tlu.edu>
2017-10-18 20:11:20 -0500

    I have modified the program to do a simple client server matrix calculation.

--- MatrixClient/MatrixClient/Program.cs
+++ MatrixClient/MatrixClient/Program.cs
@@ -5,11 +5,10 @@
     using System.Net;
     using System.Text;
     using System.Net.Sockets;
-
+    using System.Xml.Serialization;
 
     public class client
     {
-
         public static void Main()
         {
 
@@ -18,28 +17,48 @@
                 TcpClient tcpclnt = new TcpClient();
                 Console.WriteLine("Connecting.....");
 
-                tcpclnt.Connect("10.20.133.186", 8001);
-                // use the ipaddress as in the server program
-
-                Console.WriteLine("Connected");
-                Console.Write("Enter the string to be transmitted : ");
-
-                String str = Console.ReadLine();
+                tcpclnt.Connect("10.20.133.186", 8002);
                 Stream stm = tcpclnt.GetStream();
 
-                ASCIIEncoding asen = new ASCIIEncoding();
-                byte[] ba = asen.GetBytes(str);
-                Console.WriteLine("Transmitting.....");
 
-                stm.Write(ba, 0, ba.Length);
+                byte[] byteArray1;
+                byte[] byteArray2;
+                byte[] result = new byte[1];
+                // Do-while loop used for the continous flow of work and calculations.
+                do
+                {
+                    Console.WriteLine("Connected");
+                    Console.Write("Waiting for Arrays to work on.. ");
 
-                byte[] bb = new byte[100];
-                int k = stm.Read(bb, 0, 100);
+                    byteArray1 = new byte[100];
+                    byteArray2 = new byte[100];
+                    stm.Read(byteArray1, 0, 100);
+                    stm.Read(byteArray2, 0, 100);
+                    Console.WriteLine("Received array.. beginning calculations...");
 
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(bb[i]));
+                    Console.WriteLine("\nThe first array is as follows: \n");
+                    for (int i = 0; i < byteArray1.Length; i++)
+                    {
+                        Console.Write(" " + byteArray1[i]);
+                    }
+
+                    Console.WriteLine("\nThe Second array is as follows: \n");
+                    for (int i = 0; i < byteArray1.Length; i++)
+                    {
+                        Console.Write(" " + byteArray2[i]);
+                    }
+
+                    Console.WriteLine("\n\nNow beginning matrix calculation with the two give arrays..");
+                    result[0] = matrixCalculation(byteArray1, byteArray2);
+                    Console.WriteLine("The calculation has finished.");
+                    Console.WriteLine("\nNow transmitting results back to master...");
+
+                    stm.Write(result,0,1);                   
+                } while (true);
 
                 tcpclnt.Close();
+                Console.WriteLine("The program has finished, please press a key to exit.");
+                Console.ReadKey();
             }
 
             catch (Exception e)
@@ -48,5 +67,17 @@
             }
         }
 
+        private static byte matrixCalculation(byte[] byteArray1, byte[] byteArray2)
+        {
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for(int i = 0; i < byteArray1.Length; i++)
+            {
+                sum1 += byteArray1[i];
+                sum2 += byteArray2[i];
+            }
+            return (byte)(sum1 * sum2);
+        }
     }
 }
--- MatrixServer/MatrixServer/Program.cs
+++ MatrixServer/MatrixServer/Program.cs
@@ -5,42 +5,67 @@
     using System.Net;
     using System.Net.Sockets;
 
-    public class serv
-    {
+
+    public class server
+    { 
         public static void Main()
         {
             try
             {
                 IPAddress ipAd = IPAddress.Parse("10.20.133.186");
-                // use local m/c IP address, and 
-                // use the same in the client
 
-                /* Initializes the Listener */
-                TcpListener myList = new TcpListener(ipAd, 8001);
+                TcpListener myListener = new TcpListener(ipAd, 8002);
 
-                /* Start Listeneting at the specified port */
-                myList.Start();
+                myListener.Start();
+                Console.WriteLine("The server is running on port 8001.....");
+                Console.WriteLine("The local End Point is :" + myListener.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection..");
 
-                Console.WriteLine("The server is running at port 8001...");
-                Console.WriteLine("The local End point is  :" +
-                                  myList.LocalEndpoint);
-                Console.WriteLine("Waiting for a connection.....");
+                Socket mySocket = myListener.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + mySocket.RemoteEndPoint);
 
-                Socket s = myList.AcceptSocket();
-                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+                Console.WriteLine("Creating the Arrays....");
 
-                byte[] b = new byte[100];
-                int k = s.Receive(b);
-                Console.WriteLine("Recieved...");
-                for (int i = 0; i < k; i++)
-                    Console.Write(Convert.ToChar(b[i]));
+                int arraySize = 100;
+                Random rand = new Random();
 
-                ASCIIEncoding asen = new ASCIIEncoding();
-                s.Send(asen.GetBytes("The string was recieved by the server."));
-                Console.WriteLine("\nSent Acknowledgement");
-                /* clean up */
-                s.Close();
-                myList.Stop();
+                byte[] byteArray1 = new byte[arraySize];
+                byte[] byteArray2 = new byte[arraySize];
+                for (int i = 0; i < arraySize; i++)
+                {                  
+                    byteArray1[i] = Convert.ToByte(rand.Next(1, 3));
+                    byteArray2[i] = Convert.ToByte(rand.Next(1, 3));
+                }
+
+                Console.WriteLine("Created.");
+                Console.WriteLine("Printing Arrays..\n");
+                Console.WriteLine("Printing Array 1:");
+                for (int i = 0; i < arraySize; i++)
+                {
+                    Console.Write(string.Format("{0} ", byteArray1[i]));                                      
+                }
+                Console.Write("\nPrinting Array 2:\n");
+                for (int i = 0; i < arraySize; i++)
+                {
+                    Console.Write(string.Format("{0} ", byteArray1[i]));
+                }
+                Console.WriteLine("\n");
+
+                Console.WriteLine("\nSending the arrays to the worker.");
+                mySocket.Send(byteArray1);
+                mySocket.Send(byteArray2);
+                Console.WriteLine("The Arrays have been sent.");
+
+                Console.WriteLine("Waiting to recieve completed work....");
+
+                byte[] result = new byte[1];
+                int resultNumber = mySocket.Receive(result);
+                //byte number = result[0];
+
+                Console.WriteLine("Received: " + result[0] + "\n");
+
+                Console.WriteLine("The program has finished, please press a key to exit.");
+                Console.ReadKey();
 
             }
             catch (Exception e)
